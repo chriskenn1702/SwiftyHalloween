183c80789c05a5cfb1410d8f21f3aab1b6c01706
Christopher Kennedy <kennednu@bc.edu>
2023-03-19 23:30:31 -0400

    Initial Commit

---
+++ SwiftyHalloween/SwiftyHalloweenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyHalloweenApp.swift
+//  SwiftyHalloween
+//
+//  Created by Christopher Kennedy on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyHalloweenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
